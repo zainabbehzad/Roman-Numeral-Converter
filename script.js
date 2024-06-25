0d3b1d0c335c43d9349c98b77519ae06f3df2c76
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-06-25 22:15:39 +0430

    I have bring some changes in my codes, and also checked my codes in FreeCodeCamp website

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 const numberInput = document.getElementById('number');
-const convertBtn = document.getElementById('convertBtn');
+const convertButton = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 
 function convertToRoman(num) {
   if (num < 1 || num > 3999) {
-    return "Please enter a number between 1 and 3999";
+    return 'Please enter a number between 1 and 3999';
   }
 
   const romanNumerals = [
-    ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
-    ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
-    ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
-    ["", "M", "MM", "MMM"]
+    ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
+    ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
+    ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]
   ];
 
-  let result = "";
-  let digits = num.toString().split("").reverse();
-
-  for (let i = 0; i < digits.length; i++) {
-    result = romanNumerals[i][parseInt(digits[i])] + result;
+  let result = '';
+  for (const [symbol, value] of romanNumerals) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
   }
-
   return result;
 }
 
-convertBtn.addEventListener('click', () => {
-  const num = parseInt(numberInput.value);
+convertButton.addEventListener('click', () => {
+  const inputValue = numberInput.value;
+  let result;
 
-  if (isNaN(num)) {
-    output.textContent = "Please enter a valid number";
-  } else if (num < 1) {
-    output.textContent = "Please enter a number greater than or equal to 1";
-  } else if (num > 3999) {
-    output.textContent = "Please enter a number less than or equal to 3999";
+  if (inputValue === '') {
+    result = 'Please enter a valid number';
+  } else if (inputValue < 1) {
+    result = 'Please enter a number greater than or equal to 1';
+  } else if (inputValue > 3999) {
+    result = 'Please enter a number less than or equal to 3999';
   } else {
-    output.textContent = convertToRoman(num);
+    result = convertToRoman(Number(inputValue));
   }
+
+  output.textContent = result;
 });
