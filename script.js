3387091bf90cc318507b1c1abd01932217b67466
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-06-25 22:50:04 +0430

    Added some new codes in JS file

--- script.js
+++ script.js
@@ -10,32 +10,33 @@ function convertToRoman(num) {
   const romanNumerals = [
     ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
     ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
-    ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]
+    ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1,]
   ];
 
   let result = '';
-  for (const [symbol, value] of romanNumerals) {
+  romanNumerals.forEach(([symbol, value]) => {
     while (num >= value) {
       result += symbol;
       num -= value;
     }
-  }
+  });
   return result;
 }
 
 convertButton.addEventListener('click', () => {
   const inputValue = numberInput.value;
-  let result;
-
-  if (inputValue === '') {
-    result = 'Please enter a valid number';
-  } else if (inputValue < 1) {
-    result = 'Please enter a number greater than or equal to 1';
-  } else if (inputValue > 3999) {
-    result = 'Please enter a number less than or equal to 3999';
-  } else {
-    result = convertToRoman(Number(inputValue));
-  }
-
+  const result = getResult(inputValue);
   output.textContent = result;
 });
+
+function getResult(inputValue) {
+  if (inputValue === '') {
+    return 'Please enter a valid number';
+  } else if (inputValue < 1) {
+    return 'Please enter a number greater than or equal to 1';
+  } else if (inputValue > 3999) {
+    return 'Please enter a number less than or equal to 3999';
+  } else {
+    return convertToRoman(Number(inputValue));
+  }
+}
